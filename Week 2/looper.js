f32a5d6080a2dbfe25681938987fda26e67e9289
Eric Daniel Salas Concepcion <Eric@Erics-MBP.Home>
2016-11-01 19:47:08 -0600

    week2 update 2

---
+++ Week 2/looper.js
@@ -0,0 +1,51 @@
+(function(){
+
+'use strict';
+
+
+
+var list1 = ["China","Panama","Cuba"];
+
+var list2 = [
+
+{
+	country: "China",
+	when: "April"
+},
+
+{
+	country: "Panama",
+	when: "July"
+},
+
+{
+	country:"Cuba",
+	when:"July"
+}
+
+
+
+
+
+];
+
+angular.module('looperApp',[])
+.controller('LooperController',LooperController);
+
+
+CounterController.$inject = ['$scope'];
+
+function LooperController($scope){
+
+	$scope.list1  = list1;
+	$scope.list2 = list2;
+
+
+
+
+}
+
+
+
+
+})();
